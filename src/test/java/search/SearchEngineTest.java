aa5c50b2e61f210435a0b062867d810fda9a3ff8
stud-nilstes <nils.tesdal@hist.no>
2016-10-12 12:34:04 +0200

    La til tom testklasse

---
+++ src/test/java/search/SearchEngineTest.java
@@ -0,0 +1,20 @@
+package search;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author nilstes
+ */
+public class SearchEngineTest {
+    
+    public SearchEngineTest() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+}
